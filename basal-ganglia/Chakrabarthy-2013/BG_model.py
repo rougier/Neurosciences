1d39171f780e9194d3f208eabe43a6bbc93b1672
Meropi Topalidou <meropi.topalidou@inria.fr>
2014-07-29 12:18:36 +0200

    Updates

--- basal-ganglia/Chakrabarthy-2013/BG_model.py
+++ basal-ganglia/Chakrabarthy-2013/BG_model.py
@@ -1,1 +1,177 @@
-#!/usr/bin/env python# -----------------------------------------------------------------------------# Copyright (c) 2014, Nicolas P. Rougier# Distributed under the (new) BSD License.## Contributors: Nicolas P. Rougier (Nicolas.Rougier@inria.fr)#               Meropi Topalidou (Meropi.Topalidou@inria.fr)# -----------------------------------------------------------------------------from dana import *import matplotlib.pyplot as plt# Parameters# -----------------------------------------------------------------------------# Population sizen = 20pi = np.pi# Default trial durationduration = 100.0*millisecond# Default Time resolutiondt = 1.0*millisecond# Initialization of the random generator (reproductibility !)np.random.seed(1)#Domanine leveldelta = -0.2#WeightsW = np.ones((1,n))#Helpful functionsdef H(x):    return 1 if x > 0 else 0r = np.zeros((n,n,n,n))for i in range(0,n):    for j in range(0,n):        for p in range(0,n):            for q in range(0,n):                r[i,j,p,q] = np.sqrt((i-p)**2 + (j-q)**2)                      d = np.zeros((n,n))for i in range(0,n):    for p in range(0,n):        d[i,p] = np.abs(i-p)theta_D1 =0.1lamda_D1 = 5*(1/1+np.exp(-6*(delta - theta_D1)))Striatum_D1 = zeros((n,1), """du/dt = -u + V + I_ext;                                                   V = np.tanh(lamda_D1* u); ;  I_ext""")                                            ###;   Striatum_D2 = zeros((n,1), """du/dt = -u + V + I_ext;          V = np.tanh(lamda_D2* u);  lamda_D2 = 5*(1/1+np.exp(-6*(theta_D2 - delta))); theta_D2 = 0.1; I_ext""") theta_GPe = 0.1tau_GPe = 10lamda_GPe = 4 * H(theta_GPe - delta) + 1;  W_GPe = np.ones((1,n))GPe = zeros((n,n), """dx/dt = (-x +  W_lat * np.ones(U.shape) * np.sum(U) +  I_STN * w_sg + I_Str) / tau_GPe;                                   U = np.tanh(lamda_GPe * x);  I_Str = W_GPe * V_D2;                                W_lat = 1; w_sg = 1; V_D2; I_STN""")                                #W_lat# ##       #                                  #W_lat = sigma_STN * np.exp(-r^2/sigma_lat^2 ) if r < R else -1 if r =0 else 0; r; R; sigma_STN = 1; sigma_lat = 0.2;                                 theta_STN = 0.1lamda_STN = 4 * H(theta_STN - delta) + 1tau_STN = 10STN = zeros((n,n), """dx/dt = (-x +  W_lat * np.ones(U.shape) * np.sum(U) +  I_GPe * w_sg + I_Str) / tau_STN;                                   U = np.tanh(lamda_STN * x);  I_Str = W_GPe * V_D2;                                W_lat = 1; w_sg = 1; V_D2; I_GPe""")                                                                                              #((n,1), """dx/dt = (-x +W_lat * U - w_gs * I_GPe) / tau_STN;                                U = np.tanh(lamda_STN * x); W_lat = 1; w_gs = 1; ; I_GPe;; """)                                #W_lat = sigma_GPe * np.exp(-r^2/sigma_lat^2 ) if r < R else -1 if r =0 else 0;                                 #R; sigma_GPe = 1; sigma_lat = 0.2;k_x = 2*pi/n                                tau = 10A = 10 sigma =1.2; C = 0.2; tau =10;W_GPi = A * np.exp(-d**2/(2*sigma**2)) - C;W_STN_GPi = np.ones((1,n))*1./nGPi = zeros((n,1), """du/dt = (-u + np.dot(W_GPi, S) * k_x + I) / tau;                                  S = u**2 / (1 + 1/2 * k_x * np.sum(u**2));                                 I = V_D1  +  np.dot(W_STN_GPi, U_STN); U_STN ; V_D1   """)                                #parameters A, sigma, Ce{0.1,0.3}, k_x, tau are from the article from Standage                                                                     #S;                                              #                                     #                                #I                                #ConnectivityDenseConnection( Striatum_D2('V'), GPe('V_D2'), 1.0 )DenseConnection( STN('U'), GPe('I_STN'), 1.0 )DenseConnection( GPe('U'), STN('I_GPe'), 1.0 )DenseConnection( Striatum_D1('V'), GPi('V_D1'), 1.0 )DenseConnection( STN('U'), GPi('U_STN'), 1.0 )#Stimulus@clock.at(1*millisecond)def stimulus(time):    sigma = 0.4 #sigmaE{0.3,0.5}    d = np.linspace(0,n-1,n).reshape((n,1))    I = np.zeros((n,1))    I[3:7] = 8    I[12:16] = 2    #plt.plot(I)    Striatum_D1['I_ext'] = I    Striatum_D2['I_ext'] = I    # Simulation# -----------------------------------------------------------------------------run(time=duration, dt=dt)#Choiceplt.plot(GPi("S"))choice = 1 if np.sum(GPi("S")[0:10]) > np.sum(GPi("S")[10:20]) else 2print choiceplt.show()
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# Distributed under the (new) BSD License.
+# Copyright (c) 2014, Nicolas P. Rougier
+# 
+# Contributors: Nicolas P. Rougier (Nicolas.Rougier@inria.fr),
+#               Meropi Topalidou (Meropi.Topalidou@inria.fr)
+
+
+
+# Packages import
+
+
+
+from dana import *
+import matplotlib.pyplot as plt
+#%pylab --no-import-all
+
+
+# Helper functions
+
+
+
+def H(x):
+    return 1 if x > 0 else 0
+   
+
+# Simulation parameters
+
+# Population size
+n = 20
+
+# Default Time resolution
+dt = 1.0*millisecond
+
+# Default trial duration
+duration = 100.0*dt
+
+# Initialization of the random generator (reproductibility !)
+np.random.seed(1)
+
+
+# Dopamine levels
+delta = -2
+
+# Time constants
+tau_GPe = 10
+tau_STN = 10
+tau_GPi = 10
+
+theta_D1 =0.1
+theta_D2 = 0.1
+theta_GPe = 0.1
+theta_STN = 0.1
+
+
+# Compute of parameters
+lamda_D1 = 5*(1/1+np.exp(-6*(delta - theta_D1)))
+lamda_D2 = 5*(1/1+np.exp(-6*(theta_D2 - delta)))
+lamda_GPe = 4 * H(theta_GPe - delta) + 1;  
+lamda_STN = 4 * H(theta_STN - delta) + 1
+
+r = np.zeros((n,n,n,n))
+for i in range(0,n):
+    for j in range(0,n):
+        for p in range(0,n):
+            for q in range(0,n):
+                r[i,j,p,q] = np.sqrt((i-p)**2 + (j-q)**2)
+                      
+d = np.zeros((n,n))
+for i in range(0,n):
+    for p in range(0,n):
+        d[i,p] = np.abs(i-p)
+
+                           
+
+
+k_x = 2*np.pi/n                                
+A = 10 
+sigma =1.2
+C = 0.2
+#parameters A, sigma, Ce{0.1,0.3}, k_x, tau are from the article from Standage 
+
+
+W_lat = 1
+#W_lat = sigma_STN * np.exp(-r^2/sigma_lat^2 ) if r < R else -1 if r =0 else 0; r; R; sigma_STN = 1; sigma_lat = 0.2; 
+#w_sg = 1
+#w_gs = 1
+#W_GPe = np.ones((1,n))
+W_GPi = A * np.exp(-d**2/(2*sigma**2)) - C;
+#W_STN_GPi = np.ones((1,n))*1./n
+
+# Populations
+
+Striatum_D1 = zeros((n,1), """du/dt = -u + V + I_ext;    
+                              V = np.tanh(lamda_D1* u); 
+                              I_ext""")
+                                             
+Striatum_D2 = zeros((n,1), """du/dt = -u + V + I_ext;
+                              V = np.tanh(lamda_D2* u);   
+                              I_ext""") 
+
+GPe = zeros((n,n), """dx/dt = (-x +  W_lat * np.ones(U.shape) * np.sum(U) +  I_STN + I_Str) / tau_GPe;
+                      U = np.tanh(lamda_GPe * x); 
+                      I_STN; I_Str""")
+                      #I_Str = W_GPe * V_D2; 
+                                 
+                                
+STN = zeros((n,n), """dx/dt = (-x +  W_lat * np.ones(U.shape) * np.sum(U) +  I_GPe) / tau_STN;    
+                      U = np.tanh(lamda_STN * x); I_GPe""")
+                                
+
+GPi = zeros((n,1), """du/dt = (-u + np.dot(W_GPi, S) * k_x + I) / tau_GPi;  
+                                S = u**2 / (1 + 1/2 * k_x * np.sum(u**2)); 
+                                I = V_D1  +  U_STN; U_STN ; V_D1   """)
+
+# Connectivity
+
+
+DenseConnection( Striatum_D2('V'), GPe('I_Str'), np.ones((1,n)) )
+DenseConnection( STN('U'), GPe('I_STN'), 1.0 )
+DenseConnection( GPe('U'), STN('I_GPe'), 1.0 )
+DenseConnection( Striatum_D1('V'), GPi('V_D1'), 1.0 )
+DenseConnection( STN('U'), GPi('U_STN'), np.ones((1,n))*1./n )
+
+#Stimulus
+
+
+@clock.at(1*millisecond)
+def stimulus(time):
+    sigma = 0.4 #sigmaE{0.3,0.5}
+    d = np.linspace(0,n-1,n).reshape((n,1))
+    I = np.zeros((n,1))
+    first_start = 3
+    first_num_neur = 4
+    first = np.arange(first_start, first_start + first_num_neur)
+    second_start = 13
+    second_num_neur = 3
+    second = np.arange(second_start, second_start + second_num_neur)
+    I[first] = 2
+    I[second] = 4
+    Striatum_D1['I_ext'] = I
+    Striatum_D2['I_ext'] = I
+
+
+#Save GPi's activity in each tick
+
+
+Gpi = np.zeros((int(duration*1000),n))
+@after(clock.tick)
+def GP_i(t):
+    index = int(t*1000)
+    Gpi[index,:] = GPi["S"].T
+
+# Run simulation
+
+
+run(time=duration, dt=dt)
+
+#Choice
+
+choice = 1 if np.sum(GPi("S")[0:10]) > np.sum(GPi("S")[10:20]) else 2
+print choice
+print "diff = " ,np.abs(np.sum(GPi("S")[0:10]) - np.sum(GPi("S")[10:20]))
+
+#Plot of output of GPi
+
+plt.plot(Gpi[:,2],"b")
+plt.plot(Gpi[:,3],"b")
+plt.plot(Gpi[:,12],"r")
+plt.plot(Gpi[:,13],"r")
+
+plt.show()
+
