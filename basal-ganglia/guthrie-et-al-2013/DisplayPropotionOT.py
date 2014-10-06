01e37843045b9e6c28d8d88078cf7a17b83c2298
Meropi Topalidou <meropi.topalidou@inria.fr>
2014-10-06 16:19:32 +0200

    Update the Curve fitting the data

--- basal-ganglia/guthrie-et-al-2013/DisplayPropotionOT.py
+++ basal-ganglia/guthrie-et-al-2013/DisplayPropotionOT.py
@@ -1,12 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.optimize import curve_fit
+
+def fitFunc(t, a, b, c):
+    return a*np.exp(-b*t) + c
+    
+    
 file = "PropotionOfOptimalTrials.npy"
 Optimum_trials = np.load(file)
 
 trials = np.linspace(1,121,120)
-y  = 0.5 + 0.5 * (1-np.exp(-(trials - 1)/13.7)) - 0.05
-  
+fitParams, fitCovariances = curve_fit(fitFunc, trials, Optimum_trials)
+
 fig = plt.figure()
 axes = fig.add_subplot(1,1,1)
 axes.set_autoscale_on(False)
@@ -14,8 +20,8 @@ yticks = np.linspace(0,1,11)
 axes.set_xbound(0,120)
 axes.set_ybound(0,1)
 axes.set_yticks(yticks)
-axes.plot(trials, Optimum_trials)
-axes.plot(trials,y)
+axes.plot(trials, Optimum_trials, "o")
+axes.plot(trials, fitFunc(trials, fitParams[0], fitParams[1], fitParams[2]), "r", linewidth = 3)
 plt.ylabel("Proportion of optimum trials")
 plt.xlabel("Trial number")
 fig.savefig("PropotionOfOptimumTrials.pdf")
