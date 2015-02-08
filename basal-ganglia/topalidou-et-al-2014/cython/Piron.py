c923cabe2ac981440c90873707234e0756d50834
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2015-02-08 09:22:48 +0100

    Cosmetic fix

--- basal-ganglia/topalidou-et-al-2014/cython/Piron.py
+++ basal-ganglia/topalidou-et-al-2014/cython/Piron.py
@@ -15,39 +15,39 @@ C3 = np.load("Piron-C3.npy")
 C4 = np.load("Piron-C4.npy")
 
 
-# from matplotlib import rcParams
-# rcParams['xtick.direction'] = 'out'
-# rcParams['ytick.direction'] = 'out'
+from matplotlib import rcParams
+rcParams['xtick.direction'] = 'out'
+rcParams['ytick.direction'] = 'out'
 
-# plt.figure(figsize=(8,4), dpi=72, facecolor="white")
-# axes = plt.subplot(111)
-# axes.spines['right'].set_color('none')
-# axes.spines['top'].set_color('none')
-# axes.xaxis.set_ticks_position('bottom')
-# axes.spines['bottom'].set_position(('data',0))
-# axes.yaxis.set_ticks_position('left')
+plt.figure(figsize=(8,4), dpi=72, facecolor="white")
+axes = plt.subplot(111)
+axes.spines['right'].set_color('none')
+axes.spines['top'].set_color('none')
+axes.xaxis.set_ticks_position('bottom')
+axes.spines['bottom'].set_position(('data',0))
+axes.yaxis.set_ticks_position('left')
 
 
 
-# axes.plot(np.arange(120), np.mean(C1["P"],axis=0),
-#           lw=1.5, c='0.5', linestyle="--", label="HC with GPi")
-# axes.plot(np.arange(120), np.mean(C3["P"],axis=0),
-#           lw=1.5, c='0.0', linestyle="--", label="NC with GPi")
-# axes.plot(np.arange(120), np.mean(C2["P"],axis=0),
-#           lw=1.5, c='0.5', linestyle="-", label="HC without GPi")
-# axes.plot(np.arange(120), np.mean(C4["P"],axis=0),
-#           lw=1.5, c='0.0', linestyle="-", label="HC without GPi")
+axes.plot(np.arange(120), np.mean(C1["P"],axis=0),
+          lw=1.5, c='0.5', linestyle="--", label="HC with GPi")
+axes.plot(np.arange(120), np.mean(C3["P"],axis=0),
+          lw=1.5, c='0.0', linestyle="--", label="NC with GPi")
+axes.plot(np.arange(120), np.mean(C2["P"],axis=0),
+          lw=1.5, c='0.5', linestyle="-", label="HC without GPi")
+axes.plot(np.arange(120), np.mean(C4["P"],axis=0),
+          lw=1.5, c='0.0', linestyle="-", label="HC without GPi")
 
-# plt.legend(loc='lower right', frameon=False)
+plt.legend(loc='lower right', frameon=False)
 
-# plt.xlabel("Trial number")
-# plt.ylabel("Proportion of optimum trials")
+plt.xlabel("Trial number")
+plt.ylabel("Proportion of optimum trials")
 
-# plt.xlim(0,119)
-# plt.ylim(0,1.05)
-# plt.tight_layout()
-# plt.savefig("Performances.pdf")
-# plt.show()
+plt.xlim(0,119)
+plt.ylim(0,1.05)
+plt.tight_layout()
+plt.savefig("Performances.pdf")
+plt.show()
 
 
 
